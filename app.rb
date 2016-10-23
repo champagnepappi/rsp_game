2a9da2eaa0b4cbaa447b6882a8e70677b5ac71fb
kevin <kevinochieng548@gmail.com>
2016-10-23 13:13:22 +0300

    access params

--- app.rb
+++ app.rb
@@ -7,6 +7,13 @@ before do
 end
 
 get '/' do
-  
   erb :app
+end
+
+post '/' do
+ @computer_choice = @throw.sample
+ @player_choice1 = params[:rock]
+@player_choice2 = params[:paper]
+ @player_choice3 = params[:scissors]
+ redirect '/'
 end
